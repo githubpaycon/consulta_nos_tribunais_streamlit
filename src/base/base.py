8aada744e89bc1bfcc47fac60cffdd11d3284229
githubpaycon <githubpaycon@gmail.com>
2023-01-10 14:56:30 -0300

    update

--- src/base/base.py
+++ src/base/base.py
@@ -11,20 +11,16 @@ from funcsforspo_l.fpython.functions_for_py import *
 from funcsforspo_l.fselenium.functions_selenium import *
 from funcsforspo_l.fregex.functions_re import *
 from src.exceptions.exceptions import *
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import json
 import os
 
 # -- GLOBAL -- #
 URL_SUPORTE = f'https://api.whatsapp.com/send?phone=5511985640273'
-CONFIG_PATH = os.path.abspath('configs\config.json')
+CONFIG_PATH = os.path.abspath('bin\config.json')
 BASE = os.path.abspath('base')
-# JSON_CONFIG_DATA = read_json(CONFIG_PATH)
 # -- GLOBAL -- #
-
-def faz_log_st(value):
-    st.markdown(value)
 
 
 class Bot:    
@@ -54,7 +50,7 @@ class Bot:
                     "download.default_directory":  f"{self.__DOWNLOAD_DIR}",
                     "download.prompt_for_download": False,
                     "download.directory_upgrade": True,
-                    # "profile.managed_default_content_settings.images": 2,
+                    "profile.managed_default_content_settings.images": 2,
                     "safebrowsing.enabled": True}
                 
             self._options.add_experimental_option('prefs', self._PROFILE)
@@ -62,29 +58,29 @@ class Bot:
         if headless == True:
             self._options.add_argument('--headless')
             
-        self._options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        self._options.add_experimental_option("excludeSwitches", ["enable-logging", "enable-automation"])
         self._options.add_experimental_option('useAutomationExtension', False)
             
-        # self._options.add_argument(f"--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36")
-        self._options.add_argument("--disable-web-security")
-        self._options.add_argument("--allow-running-insecure-content")
-        self._options.add_argument("--disable-extensions")
-        self._options.add_argument("--start-maximized")
-        self._options.add_argument("--no-sandbox")
-        self._options.add_argument("--disable-setuid-sandbox")
-        self._options.add_argument("--disable-infobars")
-        self._options.add_argument("--disable-webgl")
-        self._options.add_argument("--disable-popup-blocking")
         self._options.add_argument('--disable-gpu')
-        self._options.add_argument('--disable-software-rasterizer')
-        self._options.add_argument('--no-proxy-server')
-        self._options.add_argument("--proxy-server='direct://'")
-        self._options.add_argument('--proxy-bypass-list=*')
-        self._options.add_argument('--disable-dev-shm-usage')
-        self._options.add_argument('--block-new-web-contents')
-        self._options.add_argument('--incognito')
-        self._options.add_argument('–disable-notifications')
-        self._options.add_argument("--window-size=1920,1080")
+        # self._options.add_argument(f"--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36")
+        # self._options.add_argument("--disable-web-security")
+        # self._options.add_argument("--allow-running-insecure-content")
+        # self._options.add_argument("--disable-extensions")
+        # self._options.add_argument("--start-maximized")
+        # self._options.add_argument("--no-sandbox")
+        # self._options.add_argument("--disable-setuid-sandbox")
+        # self._options.add_argument("--disable-infobars")
+        # self._options.add_argument("--disable-webgl")
+        # self._options.add_argument("--disable-popup-blocking")
+        # self._options.add_argument('--disable-software-rasterizer')
+        # self._options.add_argument('--no-proxy-server')
+        # self._options.add_argument("--proxy-server='direct://'")
+        # self._options.add_argument('--proxy-bypass-list=*')
+        # self._options.add_argument('--disable-dev-shm-usage')
+        # self._options.add_argument('--block-new-web-contents')
+        # self._options.add_argument('--incognito')
+        # self._options.add_argument('–disable-notifications')
+        # self._options.add_argument("--window-size=1920,1080")
         
         self.__service = Service(ChromeDriverManager().install())
         
@@ -102,3 +98,8 @@ class Bot:
 
     def quit_web(self):
         self.DRIVER.quit()
+        
+# UTILS #
+def faz_log_st(info):
+    st.markdown(f'*`{info}`*')
+# UTILS #
